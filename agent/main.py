390df65e6d084dd6ff48b477d25e96c04c98581a
Vladimir <56301056+Parkhomenko-Vladimir@users.noreply.github.com>
2022-06-24 15:05:42 +0300

    new

---
+++ agent/main.py
@@ -0,0 +1,17 @@
+import os
+import torch.multiprocessing as mp
+from parallel_env import ParallelEnv
+
+os.environ['OMP_NUM_THREADS'] = '1'
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    global_ep = mp.Value('i', 0)
+    # mp.set_start_method('forkserver')
+    env_id = 'PongNoFrameskip-v4'
+    n_threads = 4
+    n_actions = 6
+    input_shape = [4, 42, 42]
+    env = ParallelEnv(env_id = env_id, num_threads = n_threads,
+                      n_actions=n_actions, global_idx=global_ep,
+                      input_shape=input_shape)
